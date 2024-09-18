1ec598fc31d5cc9ed37f1366329b070a070e4a39
saurav-codes <selftaughtdev@pm.me>
2024-09-18 22:32:42 +0530

    updated urls

--- apps/realtime_timer/urls.py
+++ apps/realtime_timer/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 from . import htmx_views
 from .routing import websocket_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 app_name = "realtime_timer"
@@ -35,3 +37,7 @@ htmx_urlpatterns = [
 
 urlpatterns += htmx_urlpatterns
 urlpatterns += websocket_urlpatterns
+
+# static files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
