f3c10a2e90ed030437639378a1a2ef9850fa6be5
saurav-codes <selftaughtdev@pm.me>
2024-09-21 10:06:28 +0530

    workers to keep timer running and avoid being throttled

---
+++ assets/js/hack_timer_worker.js
@@ -0,0 +1,41 @@
+var fakeIdToId = {};
+onmessage = function (event) {
+	var data = event.data,
+		name = data.name,
+		fakeId = data.fakeId,
+		time;
+	if(data.hasOwnProperty('time')) {
+		time = data.time;
+	}
+	switch (name) {
+		case 'setInterval':
+			fakeIdToId[fakeId] = setInterval(function () {
+				postMessage({fakeId: fakeId});
+                console.log("posted message", fakeId);
+			}, time);
+			break;
+		case 'clearInterval':
+			if (fakeIdToId.hasOwnProperty (fakeId)) {
+				clearInterval(fakeIdToId[fakeId]);
+				delete fakeIdToId[fakeId];
+                console.log("cleared interval", fakeId);
+			}
+			break;
+		case 'setTimeout':
+			fakeIdToId[fakeId] = setTimeout(function () {
+				postMessage({fakeId: fakeId});
+				if (fakeIdToId.hasOwnProperty (fakeId)) {
+					delete fakeIdToId[fakeId];
+				}
+                console.log("posted message", fakeId);
+			}, time);
+			break;
+		case 'clearTimeout':
+			if (fakeIdToId.hasOwnProperty (fakeId)) {
+				clearTimeout(fakeIdToId[fakeId]);
+				delete fakeIdToId[fakeId];
+                console.log("cleared timeout", fakeId);
+			}
+			break;
+	}
+}
